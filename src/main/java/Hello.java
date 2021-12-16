25a8185a9f57f6752e64e3eadc6e78e4e77af642
haydarcalis <haydarcalis@gmail.com>
2021-12-16 21:10:14 +0300

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
         System.out.println("Developing hello feature");
         System.out.println("Local Repo is already published to Github");
         System.out.println("Doing more work");
+
     }
 }
